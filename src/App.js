40424d3dcd40ef9c85c0308ef39347b6621c66df
zed1025 <amit251098@yahoo.in>
2023-10-10 15:24:24 +0530

    10-10-2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import Employees from "./Employee";
+import Footer from "./Footer";
+import {useState} from "react";
+import employee from "./Employee";
 
 function App() {
+    const [selectedTeam, setSeletedTeam] = useState('TeamB');
+    const [employees, setEmployees] = useState([{
+        id: 1,
+        fullName: "Bob Jones",
+        designation: "JavaScript Developer",
+        gender: "male",
+        teamName: "TeamA"
+    },
+        {
+            id: 2,
+            fullName: "Jill Bailey",
+            designation: "Node Developer",
+            gender: "female",
+            teamName: "TeamA"
+        },
+        {
+            id: 3,
+            fullName: "Gail Shepherd",
+            designation: "Java Developer",
+            gender: "female",
+            teamName: "TeamA"
+        },
+        {
+            id: 4,
+            fullName: "Sam Reynolds",
+            designation: "React Developer",
+            gender: "male",
+            teamName: "TeamB"
+        },
+        {
+            id: 5,
+            fullName: "David Henry",
+            designation: "DotNet Developer",
+            gender: "male",
+            teamName: "TeamB"
+        },
+        {
+            id: 6,
+            fullName: "Sarah Blake",
+            designation: "SQL Server DBA",
+            gender: "female",
+            teamName: "TeamB"
+        },
+        {
+            id: 7,
+            fullName: "James Bennet",
+            designation: "Angular Developer",
+            gender: "male",
+            teamName: "TeamC"
+        },
+        {
+            id: 8,
+            fullName: "Jessica Faye",
+            designation: "API Developer",
+            gender: "female",
+            teamName: "TeamC"
+        },
+        {
+            id: 9,
+            fullName: "Lita Stone",
+            designation: "C++ Developer",
+            gender: "female",
+            teamName: "TeamC"
+        },
+        {
+            id: 10,
+            fullName: "Daniel Young",
+            designation: "Python Developer",
+            gender: "male",
+            teamName: "TeamD"
+        },
+        {
+            id: 11,
+            fullName: "Adrian Jacobs",
+            designation: "Vue Developer",
+            gender: "male",
+            teamName: "TeamD"
+        },
+        {
+            id: 12,
+            fullName: "Devin Monroe",
+            designation: "Graphic Designer",
+            gender: "male",
+            teamName: "TeamD"
+        }]);
+
+    function handleTeamSelectionChange(event) {
+        setSeletedTeam(event.target.value);
+        console.log(selectedTeam);
+    }
+
+    function handleEmployeeCardClick(event) {
+        const transformedEmployees = employees.map((employee) =>
+            employee.id === parseInt(event.currentTarget.id) ?
+                (employee.teamName === selectedTeam) ?
+                    {...employee, teamName: ''} :
+                    {...employee, teamName: selectedTeam} :
+                employee);
+        setEmployees(transformedEmployees);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        selectedTeam={selectedTeam}
+        teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}/>
+      <Employees
+        employees={employees}
+        selectedTeam={selectedTeam}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+        handleEmployeeCardClick={handleEmployeeCardClick}/>
+      <Footer />
     </div>
   );
 }
